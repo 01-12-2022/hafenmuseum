54b54fbf923f3fcd9d7df0956e6be16e1349e93a
JereIsThere <fuumax2.0@gmail.com>
2024-09-18 15:11:22 +0200

    improved landing page styling, closes #24

--- app/[locale]/TourSection.tsx
+++ app/[locale]/TourSection.tsx
@@ -1,20 +1,27 @@
 import React from "react"
 import { LandingComponentProps } from "./MuseumLandingPage"
 import styles from "./styles.module.css"
+import { Card } from "@/components/ui/card"
 
 export const TourSection: React.FC<LandingComponentProps> = ({ t, children }) => {
 
-    return (<section className={styles.toursSection}>
-        <div className={styles.toursContainer}>
-            <h2 className={styles.toursHeaderText}>{t('tours_header')}</h2>
-            <p className={styles.toursDescription}>
-                {t('tours_desc')}
-            </p>
-            <div className={styles.tourCardsContainer}>
+    return (<section
+    // className={styles.toursSection}
+    >
+        <Card
+            style={{ backgroundColor: '#F3F4F6dd', padding: 15, margin: 30 }}
+        >
+            <div className={styles.toursContainer}>
+                <h2 className={styles.toursHeaderText}>{t('tours_header')}</h2>
+                <p className={styles.toursDescription}>
+                    {t('tours_desc')}
+                </p>
+                <div className={styles.tourCardsContainer}>
 
-                {children}
+                    {children}
 
+                </div>
             </div>
-        </div>
+        </Card>
     </section>)
 }
